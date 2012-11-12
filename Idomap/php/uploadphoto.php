8a26d4993166e3cbf8c6fe4f788bd5bbee6a5890
ckubacak <ckubacak@smu.edu>
2012-11-12 16:36:28 -0600

    Changed uploadphoto.php

--- Idomap/php/uploadphoto.php
+++ Idomap/php/uploadphoto.php
@@ -3,15 +3,19 @@
 	
 	$dbh = new PDO('mysql:host=localhost; dbname=Idomap', "team", "teampass");
 	
-	if (isset($_POST['browse'])){
-		$file = $_POST['browse'];
+	if (isset($_FILES['browse']['name'])){
+		$file = $_FILES['browse']['name'];
 		$title = $_POST['title'];
 		$des = $_POST['description'];
 		$tags = $_POST['tags'];
 		$privacy = $_POST['privacy'];
 		$date = $_POST['date'];
 		
+		$imgDir = "../Images/";
+		$imgName = $imgDir.$file;
+		
 		echo "File: $file <br> Title: $title <br> Description: $des <br> Tags: $tags <br> Privacy: $privacy <br> Date: $date";
+		echo "FileDir: $imgName";
 	}
 	
 	else
